76f8bdaec84e8d0493a32e35ef962461c5692ef9
parkhangelskaya <arhangelskaya.p.d@yandex.ru>
2022-05-20 14:59:24 +0300

    lesson5

---
+++ lesson5/lesson5_1/Program.cs
@@ -0,0 +1,64 @@
+﻿/*
+ * Архангельская Полина
+ * 1. Создать программу, которая будет проверять корректность ввода логина.
+ * Корректным логином будет строка от 2 до 10 символов, содержащая только буквы латинского алфавита или цифры, при этом цифра не может быть первой:
+а) без использования регулярных выражений;
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson5_1
+{
+    internal class Program
+    {
+     
+        static bool CheckLog(string login)
+        {
+            int l = login.Length;
+            if (l >= 2 && l <= 10)                 //Check length
+            {
+                bool check = true;
+                char letter = login[0];
+                if ((Char.IsDigit(letter)))       //Check first symbol    
+                    return false;
+                for (int i = 1; i < l; i++)
+                {
+                    letter = login[i];
+                    if (!(Char.IsDigit(letter) || (login[i] >= 'A' && login[i] <= 'Z') || (login[i] >= 'a' && login[i] <= 'z'))) //|| CheckLatinLetter(letter)))
+                    {
+                        check = false;
+                        break;
+                    }
+                }
+                if (check)
+                    return true;
+            }
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter login: ");
+
+            string log = Console.ReadLine();
+
+            if (CheckLog(log))
+            {
+                Console.WriteLine("Login is correct!");
+                Console.ReadKey();
+            }
+            else
+            {
+               
+                Console.WriteLine("Invalid login!");
+                Console.ReadKey();
+            }
+            
+
+        }
+    }
+}
---
+++ lesson5/lesson5_2/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * Архангельская Полина
+ * 3. Для двух строк написать метод, определяющий, является ли одна строка перестановкой другой.
+Например: badc являются перестановкой abcd.
+ * 
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson5_2
+{
+    internal class Program
+    {
+         static bool CheckPermutation(string str1, string str2)
+        {
+            return str1.Select(Char.ToLower).OrderBy(x => x).SequenceEqual(str2.Select(Char.ToLower).OrderBy(x => x));
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please, enter the first string:");
+                string string1 = Console.ReadLine();
+            Console.WriteLine("Please, enter the second string:");
+            string string2 = Console.ReadLine();
+                if (CheckPermutation(string1, string2) == true)
+                    Console.WriteLine("strings are permutations of each other");
+                else
+                    Console.WriteLine("strings are not permutations of each other");
+
+                Console.ReadKey();
+            }
+    }
+}
