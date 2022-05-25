672314ca38b284c59e91fd72497d7c209e2baf7e
OZON\parkhangelskaya <parkhangelskaya@ozon.ru>
2022-05-25 11:23:27 +0300

    no message

---
+++ lesson8/lesson8_5/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * Архангельская Полина
+ * 
+ * Написать программу-преобразователь из CSV в XML-файл с информацией о студентах (6 урок).
+ * 
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.IO;
+
+namespace lesson8_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+
+            string[] readLines = File.ReadAllLines("..\\..\\students.csv");
+            string[] headers = { "Name", "Surname", "University", "Faculty", "Department", "Age", "Course", "Group", "City" };
+
+
+            var xml = new XElement("Students", readLines.Select(line => new XElement("StudentInfo", line.Split(';')
+          .Select((column, index) => new XElement(headers[index], column))))); //подсказку взяла туть: https://stackoverflow.com/questions/3069661/convert-csv-file-to-xml
+
+            xml.Save("..\\..\\students_new.xml");
+
+            Console.WriteLine("Преобразованный из CSV в XML-файл с информацией о студентах находится в файле students_new.xml");
+
+            Console.ReadLine();
+        }
+    }
+}
